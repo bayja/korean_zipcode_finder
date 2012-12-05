7bcca9c81c800b312dda69fc66ba7ec8b84d332a
KunHa <potato9@gmail.com>
2012-12-05 17:16:25 +0900

    version 0.1.3

--- lib/korean_zipcode_finder.rb
+++ lib/korean_zipcode_finder.rb
@@ -15,15 +15,14 @@ module KoreanZipcodeFinder
   Struct.new("KoreanZipcodeFinder", "zipcode", "zipcode_01", "zipcode_02", "address", "original_address")
   
   def self.find_zipcode(dong_name)
-    iconv = Iconv.new("euc-kr", "utf-8//IGNORE")
-    keyword = iconv.conv(dong_name.strip)
+    keyword = Iconv.new("euc-kr", "utf-8//IGNORE").iconv(dong_name.strip)
 
     response = Net::HTTP.post_form URI.parse(Configuration::URL), {'regkey' => api_key, 'target' => 'post', 'query' => keyword}
     nodes = Nokogiri::XML(response.body).css("item")
 
     nodes.map do |node|
       original_address = node.css("address").text
-      address = original_address.strip.sub(/\s(\d+)(~?)(\d+)(동|)\z/, "")
+      address = original_address.strip.sub(/\s(산|)(\d+)(~?)(\d+)(동|)\z/, "").sub(/\s(\d+)(-?)\((\d+)(~?)(\d+)\)\z/, "")
       zipcode = node.css("postcd").text
       zipcode_01 = zipcode[0..2]
       zipcode_02 = zipcode[-3..-1]
--- lib/korean_zipcode_finder/version.rb
+++ lib/korean_zipcode_finder/version.rb
@@ -1,3 +1,3 @@
 module KoreanZipcodeFinder
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
